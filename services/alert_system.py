a5b569ade9c8ac9e326a69dc3798de104185929b
ArthurOnWeb <117440022+ArthurOnWeb@users.noreply.github.com>
2025-05-29 09:37:49 +0200

    erreur syntaxe

--- services/alert_system.py
+++ services/alert_system.py
@@ -31,7 +31,7 @@ def schedule_alerts(scheduler: AsyncIOScheduler, app: Application, chat_id: int)
             trigger="interval",
             minutes=1,
             kwargs={
-                "app": Application
+                "app": Application,
                 "bot": app.bot,
                 "chat_id": chat_id,
                 "symbol": symbol,
